711f4b47f6e6c7e70f1f1939e2b79b12d19b2465
Ilan Finci <ifinci@nds.com>
2011-12-12 14:42:44 +0200

    tests on UDP & multicast
    Signed-off-by: Ilan Finci <ifinci@nds.com>

---
+++ networking/UDP-multi/multiServer.py
@@ -0,0 +1,29 @@
+import socket
+import time
+
+ANY = "0.0.0.0"
+SENDERPORT=1501
+MCAST_ADDR = "224.168.2.9"
+MCAST_PORT = 1600
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+#The sender is bound on (0.0.0.0:1501)
+sock.bind((ANY,SENDERPORT))
+#Tell the kernel that we want to multicast and that the data is sent
+#to everyone (255 is the level of multicasting)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
+sock.settimeout(0.0)
+i = 0
+while 1:
+    try:
+        time.sleep(3)
+        #send the data "hello, world" to the multicast addr: port
+        #Any subscribers to the multicast address will receive this data
+        sock.sendto("Hello World "    + str(i), (MCAST_ADDR,MCAST_PORT) );
+        i=i+1
+        data, addr = sock.recvfrom(1024)
+    except socket.error, e:
+        pass
+    else:
+        print "got answer: ", data
+    
