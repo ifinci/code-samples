f98b611af2f609c8c3852db8f36d8c61242f4b84
Ilan Finci <ifinci@nds.com>
2011-12-12 15:05:32 +0200

    adding comments

--- networking/UDP-multi/multiServer.py
+++ networking/UDP-multi/multiServer.py
@@ -12,6 +12,7 @@ sock.bind((ANY,SENDERPORT))
 #Tell the kernel that we want to multicast and that the data is sent
 #to everyone (255 is the level of multicasting)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
+#seting the sockets timeout to 0.0, to make it non-blocking, so we can check if someone sent us a reply
 sock.settimeout(0.0)
 i = 0
 while 1:
@@ -21,6 +22,9 @@ while 1:
         #Any subscribers to the multicast address will receive this data
         sock.sendto("Hello World "    + str(i), (MCAST_ADDR,MCAST_PORT) );
         i=i+1
+        #this is just for fun - test if someone sent out a reply. Note - this is not written 
+        #nicely, and will actually pick the answer on the next round (and only one answer per round, so
+        #if few clients send us answers, we'll get a queue of replies.
         data, addr = sock.recvfrom(1024)
     except socket.error, e:
         pass
